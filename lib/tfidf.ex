e3f6c36db6ece46692385ecd24b9d0529107f23c
OCannings <oli@live.co.uk>
2015-08-30 14:34:42 +0100

    chore(documentation): add some documentation!

--- lib/tfidf.ex
+++ lib/tfidf.ex
@@ -1,8 +1,49 @@
 defmodule Tfidf do
-  def calculate(word, text, corpus) when is_list(text) do
-    tfidf(word, text, corpus)
+  @moduledoc """
+  **Term frequency-inverse document frequency**
+
+  From Wikipedia:
+
+      tf–idf, short for term frequency–inverse document frequency, is a numerical
+      statistic that is intended to reflect how important a word is to a document
+      in a collection or corpus. It is often used as a weighting factor in
+      information retrieval and text mining.
+
+  Based on the blog post by Steven Loria:
+  http://stevenloria.com/finding-important-words-in-a-document-using-tf-idf/
+
+  **Basic Usage**
+
+      iex> Tfidf.calculate("dog", "nice dog dog", ["dog hat", "dog", "cat mat", "duck"])
+      0.19178804830118723
+  """
+
+  @doc """
+  Calculates the tf-idf for a given word within a tokenized list and a corpus
+  comprised of tokenized lists.
+
+      iex> Tfidf.calculate("dog", ["nice", "dog", "dog"], [["dog", "hat"], ["dog"], ["cat", "mat"], ["duck"]])
+      0.19178804830118723
+
+  """
+  def calculate(word, tokenized_text, corpus) when is_list(tokenized_text) do
+    tfidf(word, tokenized_text, corpus)
   end
 
+  @doc """
+  Calculates the tf-idf for a given word within a text and a corpus (List) of
+  texts.
+
+      iex> Tfidf.calculate("dog", "nice dog dog", ["dog hat", "dog", "cat mat", "duck"])
+      0.19178804830118723
+
+  `calculate/4` is a BYOT function (bring your own tokenizer). An optional tokenizer
+  function can be passed as the last argument to replace the default tokenizer:
+
+      iex> Tfidf.calculate("dog", "nice,dog,dog", ["dog,hat", "dog", "cat,mat", "duck"], &String.split(&1, ","))
+      0.19178804830118723
+
+  """
   def calculate(word, text, corpus, tokenize_fn \\ &tokenize(&1)) do
     text = tokenize_fn.(text)
     corpus = Enum.map(corpus, tokenize_fn)
@@ -10,12 +51,31 @@ defmodule Tfidf do
     tfidf(word, text, corpus)
   end
 
+  @doc """
+  Calculates the tf-idf for all words in a given text, returns a list
+  of {word, score} tuples.
+
+      iex> Tfidf.calculate_all("nice dog", ["dog hat", "dog", "cat mat", "duck"])
+      [{"nice", 0.6931471805599453}, {"dog", 0.14384103622589042}]
+
+  As with `Tfidf.calculate/4` an optional tokenizer function can be passed
+  as the last argument. This will be used in place of the default tokenizer.
+
+      iex> Tfidf.calculate_all("nice,dog", ["dog,hat", "dog", "cat,mat", "duck"], &String.split(&1, ","))
+      [{"nice", 0.6931471805599453}, {"dog", 0.14384103622589042}]
+  """
   def calculate_all(text, corpus, tokenize_fn \\ &tokenize(&1)) do
     Enum.map(tokenize_fn.(text) |> Enum.uniq, fn(word) ->
       {word, calculate(word, text, corpus, tokenize_fn)}
     end)
   end
 
+  @doc """
+  Returns the number of times a word appears in a given text
+
+      iex> Tfidf.word_count("dog", "cat dog cat dog")
+      2
+  """
   def word_count(word, text) do
     Enum.reduce(text, 0, fn(cur_word, acc) ->
       if cur_word == word, do: acc + 1, else: acc
